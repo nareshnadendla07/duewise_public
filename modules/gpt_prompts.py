58da84c2c7f03d0a8e47309d2e4d1b63de203062
Naresh Nadendla <nareshnadendla07@gmail.com>
2025-07-12 20:53:34 +1000

    deleted

--- modules/gpt_prompts.py
+++
@@ -1,8 +0,0 @@
-def generate_verdict_prompt(metrics_dict):
-    return f"""
-    Given the following metrics:
-    - Current Ratio: {metrics_dict['current_ratio']}
-    - Net Profit Margin: {metrics_dict['net_margin']}
-    ...
-    Provide a business health verdict, potential risks, and improvement suggestions.
-    """
